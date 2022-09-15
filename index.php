08f8513019f6f6ca47c141597862e6677b47c38b
plotiwitolp <plotiwitolp@gmail.com>
2022-09-15 17:11:09 +0400

    update

--- index.php
+++ index.php
@@ -22,6 +22,32 @@ error_reporting(E_ALL); //вывод всех ошибок
   echo __FILE__ . "<br>"; // путь до файла
   echo __LINE__ . "<br>"; // путь до строки в текущем файле
   echo PHP_VERSION, '<br>';
+  $a = 5e5;
+  $b = 5e-4;
+  echo $a . "<br>";
+  echo $b . "<br>";
+  if (is_float(3.5)) echo "3.5 - число float" . "<br>";
+  $numbers = [2, 3, 4, 5];
+  echo $numbers[0] . "<br>";
+  print_r($numbers);
+  echo "<br>";
+  // тип данных ресурс - resource - переменная содержащая ссылку на какой либо внешний ресурс
+  $book = fopen('dir/noob.txt', 'r');
+  var_dump($book);
+  echo "<br>";
+  $str1 = 'строка';
+  $int1 = 42;
+  $float1 = 42.42;
+  $bool1 = true;
+  $null1 = null;
+  var_dump($str1);
+  echo  "<br>";
+  var_dump($float1);
+  echo "<br>";
+  var_dump($bool1);
+  echo "<br>";
+  var_dump($null1);
+  echo "<br>";
 
   ?>
   <script src="./script.js"></script>
