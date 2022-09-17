1ca24e686395ca39aee9bdd02eefec622efbf509
plotiwitolp <plotiwitolp@gmail.com>
2022-09-17 15:47:21 +0400

    update

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 require_once './func.php';
-
 $x = 500;
 echo foo_fum();
 define("LOGIN", "admin");
@@ -13,18 +12,13 @@ count_up();
 echo "<br>";
 count_up();
 echo "<br>";
-
 $average = function (array $arr) {
     return array_sum($arr) / count($arr);
 };
-
 echo "среднее значение: " . $average([1, 2, 5, 6, 7]), "<br>";
-
-
 function factorial($a)
 {
     if ($a === 0) return 1;
     return $a * factorial($a - 1);
 }
-
 echo factorial(170);
